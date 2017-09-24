290761accbb0a55e983d30ee3dc34be9f6c892d0
yogo95 <yogo95@zrim-everything.eu>
2017-09-24 18:29:55 -0400

    Export classes

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 
+exports.BaseError = module.exports.BaseError = require("./lib/index").BaseError;
+exports.BaseTypeError = module.exports.BaseTypeError = require("./lib/index").BaseTypeError;
+exports.GenericBaseErrorTemplate = module.exports.GenericBaseErrorTemplate = require("./lib/index").GenericBaseErrorTemplate;
+
+exports.common = module.exports.common = require('./lib/common-errors');
+exports.security = module.exports.security = require('./lib/security-errors');
+exports.remote = module.exports.remote = require('./lib/remote-errors');
